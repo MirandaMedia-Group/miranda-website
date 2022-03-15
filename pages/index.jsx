af5fc673bea1c9ad19e3cce3b996124a0b381312
mrstikm <mrstik.martin@gmail.com>
2022-03-15 14:51:29 +0100

    eshopy in progress

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import HeroCarousel from '../components/HeroCarousel/HeroCarousel'
 import Reference from '../components/Reference/Reference'
 import Zamereni from '../components/Zamereni/Zamereni'
@@ -15,7 +16,24 @@ export default function Home() {
         <HeroCarousel></HeroCarousel>
       </section>
       <section className="container">
+        <div className={`columns col-1-4 align-center reference`}>
+            <div className="column">
+                <h2>Reference</h2>
+            </div>
+            <div className="column">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
         <Reference></Reference>
+        <div className="buttons-wrapper justify-center">
+            <Link href="/reference">
+                <a className="btn btn-secondary">Přejít na reference</a>
+            </Link>
+            <Link href="/kontakty">
+                <a className="btn btn-primary">Poptat řešení</a>
+            </Link>
+        </div>
       </section>
       <section>
         <Zamereni></Zamereni>
