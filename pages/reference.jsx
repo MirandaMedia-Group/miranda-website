f446af9bea88db45cfa6489d9e6c93dba0993eac
KlaraMiranda <klara.birnbaumova@mirandamedia.cz>
2022-03-25 11:17:30 +0100

    add spoluprace part

--- pages/reference.jsx
+++ pages/reference.jsx
@@ -1,6 +1,7 @@
 import HeroStatic from '../components/HeroStatic/HeroStatic'
 import ProjektBanner from '../components/ProjektBanner/ProjektBanner'
 import CoONasRikaji from '../components/CoONasRikaji/CoONasRikaji'
+import Reference from '../components/Reference/Reference'
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
       ipsum."
       image="/img/referenceBackground.jpg"
      />
+     <div className='container'>
+        <Reference />
+     </div>
+    
     
       <CoONasRikaji />
       <ProjektBanner />
