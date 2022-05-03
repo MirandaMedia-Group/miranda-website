a624a2432b831defe53c9a2e7498ab701e88d99c
mrstikm <mrstik.martin@gmail.com>
2022-05-03 14:31:07 +0200

    detail sluzby proces

--- components/SluzbaProgresItem/SluzbaProgresItem.jsx
+++ components/SluzbaProgresItem/SluzbaProgresItem.jsx
@@ -4,9 +4,7 @@ import styles from './SluzbaProgresItem.module.scss'
 const SluzbaProgresItem = (props) => {
     return (
         <div className={styles.progresItem}>
-            <div className={styles.imgWrapper}>
-                <Image src={props.image} width={360} height={250} alt=""></Image>
-            </div>
+            <Image src={props.image} width={360} height={250} alt="" layout='responsive'></Image>
             <div className={styles.texts}>
                 <Image src={props.icon} width={24} height={24} alt=""></Image>
                 <h2>{props.title}</h2>
