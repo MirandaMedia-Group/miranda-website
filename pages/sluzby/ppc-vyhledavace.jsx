6fd5f4fff3277145af49ee222519f22849061f39
mrstikm <mrstik.martin@gmail.com>
2022-05-03 14:21:38 +0200

    detail sluzby proces

---
+++ pages/sluzby/ppc-vyhledavace.jsx
@@ -0,0 +1,45 @@
+import HeroStatic from "../../components/HeroStatic/HeroStatic"
+import SluzbaProgres from "../../components/SluzbaProgres/SluzbaProgres"
+import SluzbaProgresItem from "../../components/SluzbaProgresItem/SluzbaProgresItem"
+import Image from "next/image"
+
+const ppcVyhledavace = () => {
+    return (
+        <>
+            <section>
+                <HeroStatic 
+                    title="PPC &amp; zbožové vyhledávače"
+                    image="/img/ppc-hero.jpg"
+                />
+            </section>
+            <section>
+                <div className="container narrow">
+                    <p>Miranda je solidní holka pro každou srandu, ale ze všeho nejvíc dá především a v první řadě na výkon, co generuje solidní keše! A proto tuní Google, 
+                        Seznam i ten zatracovanej Bing na plný obrátky celý dny a dlouhý noci. Z relevantních a padnoucích klíčovejch slov, důvtipem prošpikovanejch reklamních 
+                        textů a přesně zacílenýho, hladovýho publika dokáže uvařit lektvar růstu a načipovanýho výkonu. V tu chvíli se peníze do tvý bezedný kasy jenom hrnou, 
+                        obraty se roztáčej na plný kola a péenóčko se strachy krčí pěkně v koutě.</p>
+                </div>
+            </section>
+            <section>
+                <div className="container">
+                    <SluzbaProgres>
+                        <SluzbaProgresItem
+                            image="/"
+                            icon="/"
+                            title="Marketingový audit"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+                        />
+                        <SluzbaProgresItem
+                            image="/"
+                            icon="/"
+                            title="Marketingový audit"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+                        />
+                    </SluzbaProgres>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default ppcVyhledavace
