a93feb00c8e582897df7a1628ab464798181aec1
mrstikm <mrstik.martin@gmail.com>
2022-05-03 13:25:14 +0200

    sluzby - nutne doladit

--- components/PravaMiranda/PravaMiranda.jsx
+++ components/PravaMiranda/PravaMiranda.jsx
@@ -10,40 +10,56 @@ const PravaMiranda = () => {
             <div>
                 <div className={styles.sluzby}>
                     <div className={styles.sluzba}>
-                        <Image src="/img/ppc.jpg" width={710} height={290} layout="fill" alt=""/>
-                            <div className={styles.text}>
-                                <h4><span>Miranda</span>PPC &amp; zbožové vyhledávače</h4>
-                                <Link href="/copywriting">
-                                    <a className="btn-sm"></a>
-                                </Link>
-                            </div>
+                        <div className={`${styles.text} ${styles.miranda}`}>
+                            <h3>Miranda je ta pravá</h3>
+                            <p>Uvařit pořádně hutnej a dlouhodobej výkon v digitálu není jen tak, ale když se spojí dobrý nápad s vytrvalostí a zápalem a k tomu přidáte pár 
+                                nadšených profíků, může z toho vzniknout něco, na co budete právem pyšní! </p>
+                        </div>
                     </div>
                     <div className={styles.sluzba}>
-                        <Image src="/img/weby.jpg" width={710} height={290}  layout="fill" alt="" />
-                            <div className={styles.text}>
-                                <h4><span>Miranda</span>Weby &amp; e-shopy na klíč</h4>
-                                <Link href="/weby">
-                                    <a className="btn-sm"></a>
-                                </Link>
-                            </div>
+                        <Image src="/img/ppc.jpg" width={710} height={290} layout="fill" alt=""/>
+                        <div className={styles.text}>
+                            <h4><span>Miranda</span>PPC &amp; zbožové vyhledávače</h4>
+                            <Link href="/copywriting">
+                                <a className="btn-sm"></a>
+                            </Link>
+                        </div>
                     </div>
                     <div className={styles.sluzba}>
                         <Image src="/img/design.jpg" width={345} height={600} layout="fill" alt=""/>
-                            <div className={styles.text}>
-                                <h4><span>Miranda</span>Produktový design &amp; kreativa</h4>
-                                <Link href="/design">
-                                    <a className="btn-sm"></a>
-                                </Link>
-                            </div>
+                        <div className={styles.text}>
+                            <h4><span>Miranda</span>Produktový design &amp; kreativa</h4>
+                            <Link href="/design">
+                                <a className="btn-sm"></a>
+                            </Link>
+                        </div>
                     </div>
                     <div className={styles.sluzba}>
                         <Image src="/img/socSite.jpg" width={345} height={600} alt="" />
-                            <div className={styles.text}>
-                                <h4><span>Miranda</span>Sociální sítě</h4>
-                                <Link href="/socialni-site">
-                                    <a className="btn-sm"></a>
-                                </Link>
-                            </div>
+                        <div className={styles.text}>
+                            <h4><span>Miranda</span>Sociální sítě</h4>
+                            <Link href="/socialni-site">
+                                <a className="btn-sm"></a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.sluzba}>
+                        <Image src="/img/weby.jpg" width={710} height={290}  layout="fill" alt="" />
+                        <div className={styles.text}>
+                            <h4><span>Miranda</span>Weby &amp; e-shopy na klíč</h4>
+                            <Link href="/weby">
+                                <a className="btn-sm"></a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className={styles.sluzba}>
+                        <Image src="/img/influencer.png" width={710} height={290}  layout="fill" alt="" />
+                        <div className={styles.text}>
+                            <h4><span>Miranda</span>Influencer</h4>
+                            <Link href="/weby">
+                                <a className="btn-sm"></a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- pages/sluzby.jsx
+++ pages/sluzby.jsx
@@ -11,142 +11,148 @@ export default function Home() {
         subtitle="Miranda plnohodnotně působí jako full servis digitální - marketingová & kreativní agentura. Jeden by řekl, že toho je na samotnou agenturu poměrně hodně, ale už od počátku bylo naším cílem poskytovat služby, které spolu úzce souvisí tzv. pod jednou střechou. Tento komplexní přístup šetří našim klientům ve výsledku čas i peníze. Na své potřeby v digitální světě si snadno vystačí s jedním, silným partnerem."
         image="/img/heroBanner-min.png" 
       />
-      <div className='container sluzby'>
-        <PravaMiranda className={styles.pravaMiranda} />
-        <div className={styles.sluzby}>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>01</span>Shoptet & Shoptet Premium</h4><Link href="/shoptet-na-miru">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            <p>Když potřebuješ Shoptet na míru nebo rovnou na klíč , nenajdeš zkušenějšího parťáka nebo vlastně parťačku než právě Mirandu. Na téhle krabici jsme navrhli a postavili už hromadu e-shopů včetně Penny Marketu, Autosalonu Klokočka nebo taky on-line obchůdek pro ten zatraceně dobrej Svijanskej Pivovar.</p>
-            <p>Tenhle vymakanej systém si za víc jak 13 let svého působení dokázal přitáhnout přes 30.000 e-shopů v Český a Slovenský republice. A Miranda, tak ta je nejenom Zlatým a Prémiovým parťákem týhle super platformy, ale především místem, kde se on-lajn výkladní skříň tvýho podnikání posouvá na zcela jinej level! Pakliže hledáš pořádně vymazlenou grafiku, abys nebyl tuctovej, co je třeba naroubovat na Shoptetí střeva, naprogramovat vlastní vychytaný funkcionality, napojit jakejkoli externí systém přes apíčko nebo snad zajistit dohled nad e-shopem v režimu 24/7 přes komunikační platformu provozovanou v plně redundantním režimu s 2 databázovými servery s master-master replikou, k tomu 2x aplikační server a navrch 2x balancer zajišťující logiku automatického zapojení slave větvě při výpadku některého serveru, tak ačkoli možná nemáš šajna, co to všechno znamená, pak si tu rozhodně na tom správným místě!</p>
+      <section>
+        <div className="container sluzby">
+          <PravaMiranda className={styles.pravaMiranda} />
+        </div>
+      </section>
+      <section>
+        <div className='container'>
+          <div className={styles.sluzby}>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>01</span>Shoptet &amp; Shoptet Premium</h4><Link href="/e-shopy">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                <p>Když potřebuješ Shoptet na míru nebo rovnou na klíč , nenajdeš zkušenějšího parťáka nebo vlastně parťačku než právě Mirandu. Na téhle krabici jsme navrhli a postavili už hromadu e-shopů včetně Penny Marketu, Autosalonu Klokočka nebo taky on-line obchůdek pro ten zatraceně dobrej Svijanskej Pivovar.</p>
+                <p>Tenhle vymakanej systém si za víc jak 13 let svého působení dokázal přitáhnout přes 30.000 e-shopů v Český a Slovenský republice. A Miranda, tak ta je nejenom Zlatým a Prémiovým parťákem týhle super platformy, ale především místem, kde se on-lajn výkladní skříň tvýho podnikání posouvá na zcela jinej level! Pakliže hledáš pořádně vymazlenou grafiku, abys nebyl tuctovej, co je třeba naroubovat na Shoptetí střeva, naprogramovat vlastní vychytaný funkcionality, napojit jakejkoli externí systém přes apíčko nebo snad zajistit dohled nad e-shopem v režimu 24/7 přes komunikační platformu provozovanou v plně redundantním režimu s 2 databázovými servery s master-master replikou, k tomu 2x aplikační server a navrch 2x balancer zajišťující logiku automatického zapojení slave větvě při výpadku některého serveru, tak ačkoli možná nemáš šajna, co to všechno znamená, pak si tu rozhodně na tom správným místě!</p>
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>02</span>Software na míru</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>03</span>Produktové fotografie</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>04</span>Mobilní aplikace</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>05</span>Návrh značek</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>06</span>Firemní identita</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>07</span>Copywriting</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>08</span>Webová analytika</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>09</span>Projektové řízení</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>10</span>UX/UI</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>11</span>Datové služby</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>12</span>Školení</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
+            <div className={styles.sluzba}>
+              <div>
+                <h4><span>13</span>Podcast</h4><Link href="/#">
+                  <a className='btn-sm'></a>
+                </Link>
+              </div>
+              <div className={styles.text}>
+                #
+              </div>
+            </div>
           </div>
         </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>02</span>Software na míru</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>03</span>Produktové fotografie</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>04</span>Mobilní aplikace</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>05</span>Návrh značek</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>06</span>Firemní identita</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>07</span>Copywriting</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>08</span>Webová analytika</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>09</span>Projektové řízení</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>10</span>UX/UI</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>11</span>Datové služby</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>12</span>Školení</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-        <div className={styles.sluzba}>
-          <div>
-            <h4><span>13</span>Podcast</h4><Link href="/#">
-              <a className='btn-sm'></a>
-            </Link>
-          </div>
-          <div className={styles.text}>
-            #
-          </div>
-        </div>
-      </div>
-      </div>
+      </section>
     </>
   )
 }
