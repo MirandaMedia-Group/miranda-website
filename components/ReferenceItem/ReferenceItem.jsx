0aefa9ac3a15ebb6eb605bd51f21697e3de03680
mrstikm <mrstik.martin@gmail.com>
2022-05-05 14:51:32 +0200

    backend url changes

--- components/ReferenceItem/ReferenceItem.jsx
+++ components/ReferenceItem/ReferenceItem.jsx
@@ -10,9 +10,9 @@ const ReferenceItem = (props) => {
 
     return (
         <div className={styles.item}>
-            <Image src={'http://164.92.206.112:1337' + tmb.url} alt="" width={tmb.width} height={tmb.height} />
+            <Image src={'http://backend.mirandamedia.cz' + tmb.url} alt="" width={tmb.width} height={tmb.height} />
             <div className={styles.logo}>
-                <Image src={'http://164.92.206.112:1337' + logo.url} alt="" width={logo.width} height={logo.height} />
+                <Image src={'http://backend.mirandamedia.cz' + logo.url} alt="" width={logo.width} height={logo.height} />
             </div>
             <div className={styles.link}>
                 <Link href={`/detail-reference/${props.data.id}`} className="btn">
