c52f2093f1ad4e9775b25c01beefac84925d64ee
KlaraMiranda <klara.birnbaumova@mirandamedia.cz>
2022-05-10 13:40:35 +0200

    Penny page

--- pages/pripadova-studie/pennyDomu.jsx
+++ pages/pripadova-studie/pennyDomu.jsx
@@ -1,8 +1,10 @@
+import CaseStudy from "../../components/CaseStudy/CaseStudy";
 import PripadovkyHead from '../../components/PripadovkyHead/PripadovkyHead'
 import Image from 'next/image'
 import styles from '../../styles/pennyDomu.module.scss'
+import { fetchAPI } from "../../lib/api";
 
-const pennyDomu = () => {
+const pennyDomu = ({ reference }) => {
     return (
         <>
             <PripadovkyHead 
@@ -24,7 +26,7 @@ const pennyDomu = () => {
                 </ul>
             </section>
             <div className='container'>
-                <section className={styles.row}>
+                <section className={styles.row} id="oPenny">
                     <Image src="/img/pennyImg.jpg" width={710} height={710} />
                     <div className={styles.text}>
                         <div>
@@ -33,7 +35,7 @@ const pennyDomu = () => {
                         </div>
                     </div>
                 </section>
-                <section className={styles.cile}>
+                <section className={styles.cile} id="cile">
                     <h2>Naše společné cíle</h2>
                     <p>Jednoho dne zazvonil v Mirandě telefon, že prý Penny Market poptává digitální agenturu na tvorbu nového e-shopu. Od té chvíle se spustil kolotoč schůzek a jednání, které nás dovedly k výhradní spolupráci na vytvoření e-commerce platformy na klíč.</p>
                     <div>
@@ -46,12 +48,59 @@ const pennyDomu = () => {
                             <li>Naprogramování požadovaných funcionalit</li>
                             <li>Napojení na reklamní systémy</li>
                         </ul>
-                        <p>..a mnohem,, mnohem více</p>
+                        <p>..a mnohem, mnohem více</p>
                     </div>
                 </section>
             </div>
+            <section className={styles.background} id="oProjektu">
+                    <div className='container'>
+                        <div className={styles.quotes}><Image src="/img/michal.jpg" width={500} height={500} /></div>
+                        <div className={styles.text}>
+                            <p className={styles.bigger}>
+                            E-shop Penny Market byl pro všechny strany velkou výzvou. Už jen pro to, že jsme celou platformu vystavěli od počátku na Shoptetu. 
+                            </p>
+                            <p>Samotné zadání ze strany partnera obnášelo funkcionality a požadavky, které nebyly součástí "krabice" Shoptet. Vývojový tým v tomto ohledu sehrál nenahraditelnou roli a pomocí tzv. komunikační mezivrstvy dokázal s Shoptetem udělat zdánlivě nemožné. Jedním z požadavků bylo např. naprogramování funkcionality "Výběr časových oken s ověřením akčních cen". Doplněk zobrazuje u každé položky v košíku cenu, která platí dnes společně s cenou, která bude platit v den doručení v závislosti na akčním letáku.</p>
+                            <p>To a dlouhou řadu dalších "vychytávek" zvládla Miranda na jedničku a my děkujeme partnerovi Penny, že jsme mohli být součástí!</p>
+                            <h4>Michal Baloun</h4>
+                            <p className={styles.small}>Partner <span className={styles.green}>MirandaMedia Group s.r.o.</span></p>
+                        </div>
+                    </div>
+            </section>
+            <section className={styles.eshop} id="ukazky">
+                <div className='container'>
+                    <div className={styles.text}>
+                        <h2>Ukázka hotového e-shopu</h2>
+                        <p>Podívej se, jak vypadá e-shop Pennydomu.cz na Shoptetu. Kompletně navržená struktura a grafika na základě brand manuálu, jednotný a rozum dá, že plně responzivní design, který jsme testovali na všech možných zařízeních, co to jen šlo. Tak to projeď a rovnou něco objednej, když už tam budeš.</p>
+                        <a href="https://www.pennydomu.cz/" className='btn btn-primary'><span data-hover="Zobrazit web">Zobrazit web</span></a>
+                    </div>
+                    <div className={styles.img}>
+                        <Image src="/img/pennyRuka.png" width={1010} height={877} />
+                    </div>
+                </div>
+            </section>
+            <section className={styles.images}>
+                <Image src="/img/penny1.jpg" width={950} height={535} />
+                <Image src="/img/penny2.jpg" width={950} height={535} />
+                <Image src="/img/penny3.jpg" width={464} height={535} />
+                <Image src="/img/penny4.jpg" width={950} height={535} />
+                <Image src="/img/penny5.jpg" width={464} height={535} />
+            </section>
+            <section className="hidden">
+                    <CaseStudy reference={reference}></CaseStudy>
+                </section>
         </>
     )
 }
 
 export default pennyDomu
+
+export async function getStaticProps() {
+    const referenceRes = await fetchAPI('/references', { populate: '*' })
+  
+    return {
+        props: {
+            reference: referenceRes.data,
+        },
+        revalidate: 1,
+    }
+}
