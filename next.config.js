b6812be03d093e8aa75a81edaaf69bacd2bf5c6d
mrstikm <mrstik.martin@gmail.com>
2022-05-10 16:53:15 +0200

    sharp

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ module.exports = {
       '164.92.206.112',
       'backend.mirandamedia.cz',
       'localhost'
-  ],
+    ],
+    deviceSizes: [600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   sassOptions: {
     includePaths: ['./'],
