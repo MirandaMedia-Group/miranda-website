be7adc527b3be1c8efa2303874d9714ef00f8002
mrstikm <mrstik.martin@gmail.com>
2022-05-25 13:58:16 +0200

    reference background, swiper, ..

--- components/Reference/Reference.jsx
+++ components/Reference/Reference.jsx
@@ -1,4 +1,7 @@
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
 import ReferenceItem from '../ReferenceItem/ReferenceItem'
 
 import styles from './Reference.module.scss'
@@ -6,14 +9,52 @@ import styles from './Reference.module.scss'
 
 const Reference = (props) => { 
     const router = useRouter()
+    const [ isDesktop, setDesktop ] = useState()
 
+    useEffect( () => {
+        setDesktop(window.innerWidth > 600)
+        window.addEventListener('resize', () => {
+            setDesktop(window.innerWidth > 600)
+        })
+
+        return () => {
+            window.removeEventListener('resize', () => {
+                setDesktop(window.innerWidth > 600)
+            })
+        }
+    }, [])
     return (
         <>
-            <div className={`${styles.referenceGrid} ${router.pathname == '/' ? styles.homePage : ''}`}>
-                { props.data && props.data.map( (item, index) => {
-                    return <ReferenceItem key={index} data={item} isHomepage={router.pathname == '/' ? true : false}></ReferenceItem>
-                }) }
-            </div>
+            { !isDesktop && (
+                <Swiper
+                    modules={[Pagination]}
+                    spaceBetween={20}
+                    slidesPerView={1}
+                    pagination={{
+                        clickable: true
+                    }}
+                    breakpoints={{
+                        600: {
+                            slidesPerView: 2,
+                        }
+                    }}
+                >
+                    { props.data && props.data.map( (item, index) => {
+                        return (
+                            <SwiperSlide key={index}>
+                                <ReferenceItem data={item} isHomepage={router.pathname == '/' ? true : false}></ReferenceItem>
+                            </SwiperSlide>
+                        )
+                    }) }
+                </Swiper>
+            )}
+            { isDesktop && (
+                <div className={`${styles.referenceGrid} ${router.pathname == '/' ? styles.homePage : ''}`}>
+                    { props.data && props.data.map( (item, index) => {
+                        return <ReferenceItem key={index} data={item} isHomepage={router.pathname == '/' ? true : false}></ReferenceItem>
+                    }) }
+                </div>
+            )}
         </>
     )
 }
