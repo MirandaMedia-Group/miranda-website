629498f39ae09ba84a331e7b324cb15eff3ec7a0
mrstikm <mrstik.martin@gmail.com>
2022-05-30 13:55:48 +0200

    homepage desktop ready

--- components/ProjektBanner/ProjektBanner.jsx
+++ components/ProjektBanner/ProjektBanner.jsx
@@ -5,23 +5,16 @@ import Image from 'next/image'
 function ProjektBanner () {
     return (
         <div className={`${styles.projektBanner}`}>
-            <div className='container columns col-4-6'>
-                <div className="column">
-                    <div className={styles.rocket}>
-                        <Image src="/img/rocket.png" width={632} height={653} alt=""></Image>
-                    </div>
-                </div>
-                <div className="column">
-                    <h2>Nastartujte váš projekt</h2>
-                    <p>Miranda od počátku tvrdí, že je to hlavně o lidech, co umí vzít za práci a do každýho projektu přinesou pořádnej zápal a nadšení! Tahle energie se pak šíří napříč celým týmem. Není pak divu, že vznikaj projekty, na který můžete být právem pyšní!</p>
-                    <Link href="/kontakty">
-                        <a className="btn btn-secondary">
-                            <span data-hover="Spojit se s Mirandou">
-                                Spojit se s Mirandou
-                            </span>
-                        </a>
-                    </Link>
-                </div>
+            <div className={styles.container}>
+                <h2>Nastartujte váš projekt</h2>
+                <p>Miranda od počátku tvrdí, že je to hlavně o lidech, co umí vzít za práci a do každýho projektu přinesou pořádnej zápal a nadšení! Tahle energie se pak šíří napříč celým týmem. Není pak divu, že vznikaj projekty, na který můžete být právem pyšní!</p>
+                <Link href="/kontakty">
+                    <a className="btn btn-secondary">
+                        <span data-hover="Spojit se s Mirandou">
+                            Spojit se s Mirandou
+                        </span>
+                    </a>
+                </Link>
             </div>
         </div>
     )
