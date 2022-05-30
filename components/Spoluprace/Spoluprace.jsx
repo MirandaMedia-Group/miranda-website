99daf636ebe1be1020a71c6fda2f7d87632a8ddc
mrstikm <mrstik.martin@gmail.com>
2022-05-30 11:34:53 +0200

    proc s mirandou

--- components/Spoluprace/Spoluprace.jsx
+++ components/Spoluprace/Spoluprace.jsx
@@ -11,43 +11,32 @@ const Spoluprace = () => {
                     <p>Protože tahle agenturní dáma umí být věrná a spolehlivá každým coulem. Pro svého zákazníka udělá, co mu na očích vidí a nikdy 
                         nenechá ležet kdejakou pořádnou výzvu bokem.</p>
                     <div className={styles.mobileHidden}>
-                        <Link href="/jak-to-funguje">
+                        <Link href="/proc-s-nami">
                             <a className='btn btn-primary'>
-                                <span data-hover="Zjisti, jak to u nás funguje">
-                                    Zjisti, jak to u nás funguje
+                                <span data-hover="Více o nás">
+                                    Více o nás
                                 </span>
                             </a>
                         </Link>
                     </div>
                 </div>
-                <div className={`column ${styles.images} ${styles.mobileHidden2}`}>
-                    <div className={styles.image}>
-                        <Image src='/img/kancelar.jpg' width={450} height={450} alt="" priority={true}/>
+                <div className={`column ${styles.images}`}>
+                    <div className={`${styles.image} ${styles.image1}`}>
+                        <Image src="/img/proc-s-nami1.jpg" width={400} height={400} alt=""></Image>
                     </div>
-                    <div className={`${styles.image} ${styles.video}`}>
-                        <Image src='/img/kancelar2.jpg' width={250} height={450} alt="" priority={true}/>
+                    <div className={`${styles.image} ${styles.image2}`}>
+                        <Image src="/img/proc-s-nami2.jpg" width={508} height={508} alt=""></Image>
                     </div>
-                    <div className={styles.image}>
-                        <Image src='/img/kancelar3.jpg' width={350} height={450} alt="" priority={true}/>
-                    </div>
-                </div>
-                <div className={`column ${styles.images} ${styles.desktopHidden2}`}>
-                    <div className={styles.image}>
-                        <Image src='/img/kancelar.jpg' width={450} height={450} alt="" layout='responsive' priority={true}/>
-                    </div>
-                    <div className={`${styles.image} ${styles.video}`}>
-                        <Image src='/img/kancelar2.jpg' width={250} height={450} alt="" layout='responsive' priority={true}/>
-                    </div>
-                    <div className={styles.image}>
-                        <Image src='/img/kancelar3.jpg' width={350} height={450} alt=""  layout='responsive' priority={true}/>
+                    <div className={`${styles.image} ${styles.image3}`}>
+                        <Image src="/img/proc-s-nami3.jpg" width={300} height={300} alt=""></Image>
                     </div>
                 </div>
             </div>
             <div className={`buttons-wrapper ${styles.desktopHidden}`}>
                 <Link href="/proc-s-nami">
                     <a className='btn btn-primary'>
-                        <span data-hover="Zjisti, jak to u nás funguje">
-                            Zjisti, jak to u nás funguje
+                        <span data-hover="Více o nás">
+                            Více o nás
                         </span>
                     </a>
                 </Link>
