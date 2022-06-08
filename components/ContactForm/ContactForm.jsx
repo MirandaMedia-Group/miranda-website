917b702aa1bd05d0205dd40eda94f435f64a354a
mrstikm <mrstik.martin@gmail.com>
2022-06-08 15:28:44 +0200

    form update

--- components/ContactForm/ContactForm.jsx
+++ components/ContactForm/ContactForm.jsx
@@ -1,55 +1,36 @@
 import axios from 'axios'
 import styles from './ContactForm.module.scss'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 const ContactForm = () => {
 	let form = {
 		jmeno: useRef(null),
-		prijmeni: useRef(null),
 		email: useRef(null),
 		tel: useRef(null),
-		sluzby: {
-			premium: useRef(null),
-			grafika: useRef(null),
-			prevod: useRef(null),
-			skoleni: useRef(null),
-			programovani: useRef(null),
-			copy: useRef(null),
-			ppc: useRef(null),
-			foto: useRef(null),
-			foto360: useRef(null),
-		},
 		zprava: useRef(null),
 	}
 	let token = 'Token!'
+	const [formSent, setFormSent] = useState(false)
+	const [formSending, setFormSending] = useState(false)
 
 	const sendForm = async (e) => {
 		e.preventDefault()
 
 		let data = {
 			jmeno: form.jmeno.current.value,
-			prijmeni: form.prijmeni.current.value,
 			email: form.email.current.value,
 			tel: form.tel.current.value,
-			sluzby: {
-				premium: form.sluzby.premium.current.checked,
-				grafika: form.sluzby.grafika.current.checked,
-				prevod: form.sluzby.prevod.current.checked,
-				skoleni: form.sluzby.skoleni.current.checked,
-				programovani: form.sluzby.programovani.current.checked,
-				copy: form.sluzby.copy.current.checked,
-				ppc: form.sluzby.ppc.current.checked,
-				foto: form.sluzby.foto.current.checked,
-				foto360: form.sluzby.foto360.current.checked,
-			},
 			zprava: form.zprava.current.value,
 		}
 
 		try {
+			setFormSending(true)
 			const resolve = await axios.post('http://164.92.206.112:1337/api/ezforms/submit', {
 				token,
 				formData: JSON.stringify(data),
 			})
+			setFormSending(false)
+			setFormSent(true)
 			console.log(resolve)
 		} catch (e) {
 			console.log(e)
@@ -75,64 +56,29 @@ const ContactForm = () => {
 							</a>
 						</div>
 						<div className={styles.column}>
-							<form onSubmit={sendForm}>
-								<div className={styles.formRow}>
-									<input type='text' name='jmeno' id='jmeno' placeholder='Jméno a příjmení' ref={form.jmeno} />
-								</div>
-								<div className={styles.formRow}>
-									<input type='email' name='email' id='email' placeholder='E-mail' ref={form.email} />
-								</div>
-								<div className={styles.formRow}>
-									<input type='tel' name='tel' id='tel' placeholder='Telefon' ref={form.tel} />
-								</div>
-
-								{/* <div className={styles.checkboxes}>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="premium" id="premium" ref={form.sluzby.premium}/>
-                                        <label htmlFor="premium">Shoptet PREMIUM</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="grafika" id="grafika" ref={form.sluzby.grafika}/>
-                                        <label htmlFor="grafika">Grafika na míru</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="prevod" id="prevod" ref={form.sluzby.prevod}/>
-                                        <label htmlFor="prevod">Převod e-shopu od konkurence</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="skoleni" id="skoleni" ref={form.sluzby.skoleni}/>
-                                        <label htmlFor="skoleni">Školení a konzultace</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="programovani" id="programovani" ref={form.sluzby.programovani}/>
-                                        <label htmlFor="programovani">Programování a kódování</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="copy" id="copy" ref={form.sluzby.copy}/>
-                                        <label htmlFor="copy">Copywriting</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="ppc" id="ppc" ref={form.sluzby.ppc}/>
-                                        <label htmlFor="ppc">PPC a zbožové vyhledávače</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="foto" id="foto" ref={form.sluzby.foto}/>
-                                        <label htmlFor="foto">2D produktové fotky</label>
-                                    </span>
-                                    <span className={styles.group}>
-                                        <input type="checkbox" name="360" id="360" ref={form.sluzby.foto360}/>
-                                        <label htmlFor="360">360° produktové fotky</label>
-                                    </span>
-                                </div> */}
-								<textarea name='zprava' id='zprava' rows='5' placeholder='Váš vzkaz' ref={form.zprava}></textarea>
-								<div className={styles.formRow}>
-									<input type='checkbox' name='consent' id='consent' readOnly />
-									<label htmlFor='consent'>Odesláním souhlasíte s Podmínkami zpracovaní osobních údajů</label>
-								</div>
-								<button type='submit' className='btn btn-primary'>
-									<span data-hover='Odeslat poptávku'>Odeslat poptávku</span>
-								</button>
-							</form>
+							{!formSent && !formSending && (
+								<form onSubmit={sendForm}>
+									<div className={styles.formRow}>
+										<input type='text' name='jmeno' id='jmeno' placeholder='Jméno a příjmení' ref={form.jmeno} />
+									</div>
+									<div className={styles.formRow}>
+										<input type='email' name='email' id='email' placeholder='E-mail' ref={form.email} />
+									</div>
+									<div className={styles.formRow}>
+										<input type='tel' name='tel' id='tel' placeholder='Telefon' ref={form.tel} />
+									</div>
+									<textarea name='zprava' id='zprava' rows='5' placeholder='Váš vzkaz' ref={form.zprava}></textarea>
+									<div className={styles.formRow}>
+										<input type='checkbox' name='consent' id='consent' readOnly />
+										<label htmlFor='consent'>Odesláním souhlasíte s Podmínkami zpracovaní osobních údajů</label>
+									</div>
+									<button type='submit' className='btn btn-primary'>
+										<span data-hover='Odeslat poptávku'>Odeslat poptávku</span>
+									</button>
+								</form>
+							)}
+							{formSending && !formSent && <div>Loader!!</div>}
+							{formSent && !formSending && <div>Formulář byl v pořádku odeslán, naši accounti se Vám co nejdříve ozvou.</div>}
 						</div>
 					</div>
 				</div>
